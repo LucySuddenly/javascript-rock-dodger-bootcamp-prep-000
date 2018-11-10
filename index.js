46a1512e8d0e16fc958c99abba3d70abbb2486e2
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-10 00:48:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,8 +69,7 @@ function createRock(x) {
    * it to GAME and move it downwards.
    */
   GAME.appendChild(rock)
-  moveRock(rock)
-  return rock
+  
 
   /**
    * This function moves the rock. (2 pixels at a time
